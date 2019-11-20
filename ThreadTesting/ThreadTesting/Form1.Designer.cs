0b3a08b4d5e0b7332ac42ef197581c29f372baf5
Jian <jiceberg420@qq.com>
2019-11-20 09:42:49 +0800

    Backgournd worker examples

---
+++ ThreadTesting/ThreadTesting/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace ThreadTesting
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.barProgress = new System.Windows.Forms.ProgressBar();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.chkTestError = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // barProgress
+            // 
+            this.barProgress.Location = new System.Drawing.Point(12, 186);
+            this.barProgress.Name = "barProgress";
+            this.barProgress.Size = new System.Drawing.Size(447, 23);
+            this.barProgress.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.btnStart.Location = new System.Drawing.Point(15, 50);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(163, 51);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(277, 50);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(182, 51);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(12, 170);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(37, 13);
+            this.lblStatus.TabIndex = 3;
+            this.lblStatus.Text = "Status";
+            // 
+            // chkTestError
+            // 
+            this.chkTestError.AutoSize = true;
+            this.chkTestError.Location = new System.Drawing.Point(15, 107);
+            this.chkTestError.Name = "chkTestError";
+            this.chkTestError.Size = new System.Drawing.Size(163, 17);
+            this.chkTestError.TabIndex = 4;
+            this.chkTestError.Text = "Throw Error before complete.";
+            this.chkTestError.UseVisualStyleBackColor = true;
+            this.chkTestError.CheckedChanged += new System.EventHandler(this.chkTestError_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(485, 240);
+            this.Controls.Add(this.chkTestError);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.barProgress);
+            this.Name = "Form1";
+            this.Text = "Test Background Worker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar barProgress;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.CheckBox chkTestError;
+    }
+}
+
---
+++ ThreadTesting/ThreadTesting/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ThreadTesting
+{
+    public partial class Form1 : Form
+    {
+        BackgroundWorker bw;
+        delegate void UpdateStatus(string status);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if(bw!=null)
+            {
+                if (bw.IsBusy) bw.CancelAsync();
+            }
+
+            bw = new BackgroundWorker();
+            bw.WorkerReportsProgress = true;
+            bw.WorkerSupportsCancellation = true;
+            bw.DoWork += Bw_DoWork;
+            bw.ProgressChanged += Bw_ProgressChanged;
+            bw.RunWorkerCompleted += Bw_RunWorkerCompleted;
+
+            int loopTimes = 100;
+            bool createError = chkTestError.Checked;
+
+            List<object> args = new List<object>();
+            args.Add(loopTimes);
+            args.Add(new UpdateStatus(UpdateProgressStatus));
+            args.Add(createError);
+
+            bw.RunWorkerAsync(args);
+        }
+
+  
+
+        private void Bw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            var args = e.Argument as List<object>;
+            var loopTimes = (int)args[0];
+            var updateStatus = (UpdateStatus)args[1];
+            var createError = (bool)args[2];
+            var worker = (BackgroundWorker)sender;
+
+            for (int i = 0; i < loopTimes; i++)
+            {
+                if (worker.CancellationPending)
+                {
+                    e.Cancel = true;
+                    break;
+                }
+                System.Threading.Thread.Sleep(100);
+                string msg = $"Current progress is {i}";
+                worker.ReportProgress(i*100/loopTimes, msg);
+                updateStatus?.Invoke(msg);
+            }
+
+            if (createError) throw new Exception("An error test!");
+            worker.ReportProgress(100);
+        }
+
+        private void Bw_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            barProgress.Value = e.ProgressPercentage * barProgress.Maximum / 100;
+            lblStatus.Text = e.UserState as string;
+        }
+        private void UpdateProgressStatus(string status)
+        {
+            //lblStatus.Text = status; //This will trigger the error of UI update not in its created thread.
+        }
+        private void Bw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+            {
+                lblStatus.Text = "User Cancelled!";
+            }
+            else if (e.Error != null)
+            {
+                lblStatus.Text = $"Error! {e.Error.Message}";
+            }
+            else
+            {
+                lblStatus.Text = "Accomplished!";
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if (bw.IsBusy)
+                bw.CancelAsync();
+        }
+
+        private void chkTestError_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
