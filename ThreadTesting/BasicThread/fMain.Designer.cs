be58910dd715c923395079df6b16bbcef27bfd49
Jian <jiceberg420@qq.com>
2019-11-20 10:52:11 +0800

    BasicThread example created.

---
+++ ThreadTesting/BasicThread/fMain.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace BasicThread
+{
+    partial class fMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 25);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(140, 53);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start\r\n( Click me several times )";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(177, 25);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 53);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "TBD";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(12, 104);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtOutput.Size = new System.Drawing.Size(875, 324);
+            this.txtOutput.TabIndex = 2;
+            // 
+            // fMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(899, 464);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnStart);
+            this.Name = "fMain";
+            this.Text = "Basic Thread";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtOutput;
+    }
+}
+
---
+++ ThreadTesting/BasicThread/fMain.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace BasicThread
+{
+    public partial class fMain : Form
+    {
+        delegate void UpdateUI(string msg);
+        private int threadCount = 0;
+        public fMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //https://www.pluralsight.com/guides/how-to-write-your-first-multi-threaded-application-with-c
+
+            var a = new Thread(CountDownThread);
+            a.Name = $"Thread{this.threadCount++,2}";
+            a.Start();
+            AppendOutput($"{Thread.CurrentThread.Name}: {a.Name} started. status is {a.ThreadState.ToString()}");
+
+        }
+
+        private void CountDownThread()
+        {
+            for (int i = 10; i >=0; i--)
+            {
+
+                int interval_ms = 333;
+                //Thread.Sleep(interval_ms);
+                Thread.Sleep(TimeSpan.FromMilliseconds(interval_ms));
+
+                //To update the UI created at another thread,
+                //we have to Invoke it.
+                string msg = $"{Thread.CurrentThread.Name}: {i}";
+                txtOutput.Invoke(new UpdateUI(AppendOutput), msg);
+            }
+
+        }
+
+
+
+        private void AppendOutput(string msg)
+        {
+            txtOutput.Text += $"{msg}\r\n";
+            txtOutput.Select(txtOutput.Text.Length, 0);
+            txtOutput.ScrollToCaret();
+        }
+    }
+}
